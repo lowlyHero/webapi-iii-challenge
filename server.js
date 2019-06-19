4ddb8a98d0c3479043ead5f1cfa22c7a3fcffa29
lowlyHero <e.nunn1009@gmail.com>
2019-06-19 17:40:19 -0400

    added error handlers in server

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan');
 
 const server = express();
+
+// Global Middleware
+server.use(express.json());
+server.use(helmet());
+server.use(logger('dev'));
+
+//custom middleware
 
 server.get('/', (req, res) => {
   res.send(`<h2>Sanity Check</h2>`)
 });
 
-//custom middleware
+// the next param is a function that points to the next middleware
+// calling next() will signal that middleware has finished & should call next middleware function
 
 function logger(req, res, next) {
 
 };
+
+
+//error handling
+server.use((error, req, res, next) => {
+  res.status(400).json({
+    message: 'Bad request friend! Try again.', error
+  });
+})
 
 module.exports = server;
